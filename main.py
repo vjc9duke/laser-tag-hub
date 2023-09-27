c9c4221dc173d8d85d9b7fd3d616d75fae4ab9da
Vincent <vjc9@duke.edu>
2023-09-27 13:54:27 -0400

    starter application

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMessageBox
+
+def dialog():
+    mbox = QMessageBox()
+
+    mbox.setText("Your allegiance has been noted")
+    mbox.setDetailedText("You are now a disciple and subject of the all-knowing Guru")
+    mbox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            
+    mbox.exec_()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(700,700)
+    w.setWindowTitle("Guru99")
+    
+    label = QLabel(w)
+    label.setText("Behold the Guru, Guru99")
+    label.move(280,380)
+    label.show()
+
+    btn = QPushButton(w)
+    btn.setText('Beheld')
+    btn.move(300,400)
+    btn.show()
+    btn.clicked.connect(dialog)
+
+    
+    w.show()
+    sys.exit(app.exec_())
