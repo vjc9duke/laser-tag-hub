0fddc21330cbf7de8bd77ba886e28a9025896cc1
Vincent <vjc9@duke.edu>
2023-10-18 01:42:49 -0400

    serial fix

--- LORA.py
+++ LORA.py
@@ -23,12 +23,13 @@ async def ATcmd(cmd: str = '') -> int:
     count : int  = await aio.write_async(bytes(command, 'utf8'))
     return count
 
-# Define the LoRa serial port
-# lora = serial.Serial("/dev/ttyS0", 9600, timeout=0.5)
+async def main_func():
+    # Define the LoRa serial port
+    # lora = serial.Serial("/dev/ttyS0", 9600, timeout=0.5)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(True)
-GPIO.setup(self.RST,GPIO.OUT,initial=GPIO.HIGH) # the default anyway
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(True)
+    GPIO.setup(self.RST,GPIO.OUT,initial=GPIO.HIGH) # the default anyway
 
 # Send configuration commands to the LoRa module
 # lora.write(b'AT+ADDRESS=1\r\n')
@@ -38,27 +39,29 @@ GPIO.setup(self.RST,GPIO.OUT,initial=GPIO.HIGH) # the default anyway
 # lora.write(b'AT+BAND=433\r\n')
 # time.sleep(0.1)
 
-await ATcmd('ADDRESS=1')
-await ATcmd('NETWORKID=5')
-await ATcmd('BAND=433')
+    await ATcmd('ADDRESS=1')
+    await ATcmd('NETWORKID=5')
+    await ATcmd('BAND=433')
 
-try:
-    while True:
-        # if lora.in_waiting:
-            # incomingString = lora.readline().decode("utf-8").strip()
-            # print(incomingString)
+    try:
+        while True:
+            # if lora.in_waiting:
+                # incomingString = lora.readline().decode("utf-8").strip()
+                # print(incomingString)
 
-            # data = incomingString.split(',')
-            # if len(data) >= 4:
-               #  received_data = data[3]
-                # print("Received data:", received_data)
-        if aio.in_waiting > 0:
-            data = await aio.read_async(size=1)
-            print(data)
+                # data = incomingString.split(',')
+                # if len(data) >= 4:
+                   #  received_data = data[3]
+                    # print("Received data:", received_data)
+            if aio.in_waiting > 0:
+                data = await aio.read_async(size=1)
+                print(data)
 
-except KeyboardInterrupt:
-    print("Keyboard Interrupt. Exiting...")
-except Exception as e:
-    print("Error:", str(e))
-finally:
-    lora.close()
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt. Exiting...")
+    except Exception as e:
+        print("Error:", str(e))
+    finally:
+        lora.close()
+
+main_func()
