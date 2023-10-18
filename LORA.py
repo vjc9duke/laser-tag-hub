19c09713f976eefab2dd16678008f1e462d5c6cc
Vincent <vjc9@duke.edu>
2023-10-18 01:18:20 -0400

    serial fix

--- LORA.py
+++ LORA.py
@@ -1,42 +1,21 @@
 import serial
 import time
-import RPi.GPIO as GPIO
 
-# Set up GPIO pins
-GPIO.setmode(GPIO.BCM)
-TX_PIN = 14
-RX_PIN = 15
-GPIO.setup(TX_PIN, GPIO.OUT)
-GPIO.setup(RX_PIN, GPIO.IN)
+# Define the LoRa serial port
+lora = serial.Serial("/dev/ttyS0", 9600, timeout=0.5)
 
-# Configure the serial port with GPIO pins
-ser = serial.Serial(
-    port="/dev/ttyS0",
-    baudrate=9600,
-    timeout=0.5,
-    xonxoff=False,
-    rtscts=False,
-    dsrdtr=False,
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-)
+# Send configuration commands to the LoRa module
+lora.write(b'AT+ADDRESS=1\r\n')
+time.sleep(0.1)
+lora.write(b'AT+NETWORKID=5\r\n')
+time.sleep(0.1)
+lora.write(b'AT+BAND=433\r\n')
+time.sleep(0.1)
 
 try:
-    ser.open()
-    print("Serial port opened successfully.")
-
-    # Send configuration commands to the LoRa module
-    ser.write(b"AT+ADDRESS=1\r\n")
-    time.sleep(0.1)
-    ser.write(b"AT+NETWORKID=5\r\n")
-    time.sleep(0.1)
-    ser.write(b"AT+BAND=433\r\n")
-    time.sleep(0.1)
-
     while True:
-        if ser.in_waiting:
-            incomingString = ser.readline().decode("utf-8").strip()
+        if lora.in_waiting:
+            incomingString = lora.readline().decode("utf-8").strip()
             print(incomingString)
 
             data = incomingString.split(',')
@@ -49,7 +28,4 @@ except KeyboardInterrupt:
 except Exception as e:
     print("Error:", str(e))
 finally:
-    if ser.is_open:
-        ser.close()
-        print("Serial port closed.")
-    GPIO.cleanup()
+    lora.close()
